f1fc5e8688c10029f116b41344affe063d91804f
Lucas Prins <lucas2002prins@gmail.com>
2024-09-22 15:13:54 +0200

    feat: update 404 page

--- apps/web/src/app/404.tsx
+++ apps/web/src/app/404.tsx
@@ -1,9 +1,17 @@
+import { Flex, Logo, Text } from '@align/ui'
 import { useTranslation } from 'react-i18next'
 
 const PageNotFound: React.FC = () => {
   const { t } = useTranslation(['common'])
 
-  return <main>{t('common:404.message')}</main>
+  return (
+    <Flex className="AccessDenied" align="center" justify="center">
+      <Flex direction="column" align="center" gap={5}>
+        <Logo className="Logo" />
+        <Text align="center">{t('common:404.message')}</Text>
+      </Flex>
+    </Flex>
+  )
 }
 
 export default PageNotFound
